11c147e137c3976cd28ea5bb7b4f1abd727357ce
Paun Cristian <pauncristianafc.@gmail.com>
2025-11-20 23:10:24 +0200

    Fixed FastAPI dependencies to scratch image in Docker Edited .gitignore file
    Signed-off-by: Paun Cristian <pauncristianafc.@gmail.com>

--- http-python3.12-FastAPI-0.121.3/server.py
+++ http-python3.12-FastAPI-0.121.3/server.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,6 +10,5 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
